f889f59e71cb59b1db24431706b2999d2e87d743
Deep-74 <deepghuge74@gmail.com>
2023-08-05 19:05:40 +0530

    typos

--- client/src/components/ProtectedPage.js
+++ client/src/components/ProtectedPage.js
@@ -80,7 +80,7 @@ function ProtectedPage({ children }) {
             className="text-2xl text-white cursor-pointer"
             onClick={() => navigate("/")}
           >
-           Vender app
+           Vendor app
           </h1>
 
           <div className="bg-white py-2 px-5 rounded flex gap-1 items-center">
